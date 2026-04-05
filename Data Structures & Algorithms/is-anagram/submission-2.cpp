c630e610000b104e44539b5fb36bcef9c01d32c4
Akash Kumar <ms1230942@mse.iitd.ac.in>
2026-04-06 01:22:35 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char, int> a,b;
+        for(int i=0;i<s.size();++i){
+            a[s[i]]++;
+            b[t[i]]++;
+        }
+        return(a==b);
+    }
+};
